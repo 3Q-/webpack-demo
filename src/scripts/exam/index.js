5df8117bc38f98a0f503a4c3754adf563ef561f8
qiyong.xie <yongjy@126.com>
2015-11-09 12:02:24 +0800

    git ignore .ds-_store

--- src/scripts/exam/index.js
+++
@@ -1,9 +0,0 @@
-
-//require('./style.css');
-'use strict';
-require('../../styles/style.css');
-
-var ele = require('../modules/dev');
-console.log('exam/index');
-//console.log(ele(),1);
-document.body.appendChild(ele());
--- src/scripts/modules/common.js
+++
@@ -1,8 +0,0 @@
-
-'use strict';
-module.exports = function(){
-    var ele = document.createElement('h1');
-    ele.innerHTML = 'Fuck Webpack\'s Family At All~~~';
-    console.log('modules/Common');
-    return ele;
-};
